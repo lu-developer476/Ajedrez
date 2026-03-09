2018288299e50506ae3936f757e5b8eafe4e0eb6
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-08 23:06:03 -0300

    Update settings.py

--- cyborg_chess/settings.py
+++ cyborg_chess/settings.py
@@ -65,13 +65,6 @@ DATABASES = {
         ssl_require=not DEBUG,
     )
 }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 AUTH_PASSWORD_VALIDATORS = []
 
