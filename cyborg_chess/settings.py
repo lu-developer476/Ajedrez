2164f5438557f1025c646760cd9abe2928945807
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-08 23:02:18 -0300

    Update settings.py

--- cyborg_chess/settings.py
+++ cyborg_chess/settings.py
@@ -56,15 +56,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cyborg_chess.wsgi.application'
 
-DATABASE_URL = os.getenv('DATABASE_URL', '')
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=not DEBUG,
-        )
-    }
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
+        conn_max_age=600,
+        ssl_require=not DEBUG,
+    )
+}
 else:
     DATABASES = {
         'default': {
